3108f1267ef3a58b66b92d9683df38bc2695f6f4
Serhii <aserhii@protonmail.com>
2019-10-28 12:04:44 +0200

    Pass task1

--- Task1/test.py
+++ Task1/test.py
@@ -1,5 +1,14 @@
-# Write your tests here
+from .task import get_temperature_closest_to_zero
+from .data import temperatures
+import pytest
 
 
 def test_get_temperature_closest_to_zero():
-    assert 1 == 1
+    assert 0.5 == get_temperature_closest_to_zero(temperatures)
+    assert get_temperature_closest_to_zero([]) == 0
+    assert get_temperature_closest_to_zero([-1, 2, 4, 0.2, 5, 3, -2, -0.1]) == -0.1
+    assert get_temperature_closest_to_zero([1,2,3,4,5,0,11]) == 0
+    assert get_temperature_closest_to_zero([11,-11]) == 11
+    assert get_temperature_closest_to_zero([120,-3,55]) == -3
+    assert get_temperature_closest_to_zero([1,-1,0.1]) == 0.1
+    assert get_temperature_closest_to_zero([3,-3]) == 3
