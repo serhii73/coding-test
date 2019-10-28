45b5629b63df1f34d6ce9e55e69f152d12ab3747
Serhii <aserhii@protonmail.com>
2019-10-28 13:02:56 +0200

    Add task2

--- Task2/task.py
+++ Task2/task.py
@@ -1,5 +1,41 @@
-# Write your code here
+import requests
+from bs4 import BeautifulSoup
+from word2number import w2n
 
 
 def get_products_from_page(url):
-    return []
+    """Return data from all books."""
+
+    def get_data_from_book(book):
+        """Return data from one book."""
+        src_img = book.find("img").get("src")
+        src_img = src_img.replace("../", "")
+        image = "http://books.toscrape.com/" + src_img
+
+        in_stock = False
+        in_stock_or_not = book.find("p", {"class", "instock"}).text
+        if "In stock" in in_stock_or_not:
+            in_stock = True
+
+        name = book.find("h3").find("a").text
+
+        price = book.find("p", {"class", "price_color"}).text
+        price = price.replace("Â", "")
+
+        rating = book.find("p", {"class", "star-rating"}).get("class")[1]
+        rating = w2n.word_to_num(rating)
+
+        return {
+            "image": image,
+            "in_stock": in_stock,
+            "name": name,
+            "price": price,
+            "rating": rating,
+        }
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    books = soup.find_all("article", {"class", "product_pod"})
+
+    result = list(map(get_data_from_book, books))
+    return result
--- Task2/test.py
+++ Task2/test.py
@@ -1,5 +1,32 @@
-# Write your tests here
+import pytest
+
+from .task import get_products_from_page
+
+result_page_2 = get_products_from_page(
+    "http://books.toscrape.com/catalogue/page-2.html"
+)
+result_page_1 = get_products_from_page("http://books.toscrape.com/index.html")
 
 
 def test_get_products_from_page():
-    pass
+    assert result_page_2[0] == {
+        "image": "http://books.toscrape.com/media/cache/5d/72/5d72709c6a7a9584a4d1cf07648bfce1.jpg",
+        "in_stock": True,
+        "name": "In Her Wake",
+        "price": "£12.84",
+        "rating": 1,
+    }
+    assert result_page_1[0] == {
+        "image": "http://books.toscrape.com/media/cache/2c/da/2cdad67c44b002e7ead0cc35693c0e8b.jpg",
+        "in_stock": True,
+        "name": "A Light in the ...",
+        "price": "£51.77",
+        "rating": 3,
+    }
+    assert result_page_1[1] == {
+        "image": "http://books.toscrape.com/media/cache/26/0c/260c6ae16bce31c8f8c95daddd9f4a1c.jpg",
+        "in_stock": True,
+        "name": "Tipping the Velvet",
+        "price": "£53.74",
+        "rating": 1,
+    }
