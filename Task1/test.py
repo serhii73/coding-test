cf689658425cbd359f5e6ec703d13899c3aac2e5
Serhii <aserhii@protonmail.com>
2019-10-28 12:08:25 +0200

    Apply isort and black for py files

--- Task1/test.py
+++ Task1/test.py
@@ -1,14 +1,15 @@
-from .task import get_temperature_closest_to_zero
-from .data import temperatures
 import pytest
+
+from .data import temperatures
+from .task import get_temperature_closest_to_zero
 
 
 def test_get_temperature_closest_to_zero():
     assert 0.5 == get_temperature_closest_to_zero(temperatures)
     assert get_temperature_closest_to_zero([]) == 0
     assert get_temperature_closest_to_zero([-1, 2, 4, 0.2, 5, 3, -2, -0.1]) == -0.1
-    assert get_temperature_closest_to_zero([1,2,3,4,5,0,11]) == 0
-    assert get_temperature_closest_to_zero([11,-11]) == 11
-    assert get_temperature_closest_to_zero([120,-3,55]) == -3
-    assert get_temperature_closest_to_zero([1,-1,0.1]) == 0.1
-    assert get_temperature_closest_to_zero([3,-3]) == 3
+    assert get_temperature_closest_to_zero([1, 2, 3, 4, 5, 0, 11]) == 0
+    assert get_temperature_closest_to_zero([11, -11]) == 11
+    assert get_temperature_closest_to_zero([120, -3, 55]) == -3
+    assert get_temperature_closest_to_zero([1, -1, 0.1]) == 0.1
+    assert get_temperature_closest_to_zero([3, -3]) == 3
